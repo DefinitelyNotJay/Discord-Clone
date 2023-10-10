2d9914580b1e72119315dca8fbd910c6a1b75f7b
Tendou26 <jjbrathers3@gmail.com>
2023-10-10 10:25:56 +0700

    +dark theme toggle
    using shadcn darktheme, dropdown

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from '@clerk/nextjs';
+import { ModeToggle } from '@/components/ui/mode-toggle';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <UserButton afterSignOutUrl="/"></UserButton>
+      <ModeToggle></ModeToggle>
     </div>
   );
 };
